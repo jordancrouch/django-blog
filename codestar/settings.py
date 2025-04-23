077582ddfa16ebdb1ab67d4b69bec5185257923a
Jordan Crouch <107533237+jordancrouch@users.noreply.github.com>
2025-04-23 15:26:00 +0100

    Feat: add post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -88,6 +89,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://*.127.0.0.1:8000//",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
